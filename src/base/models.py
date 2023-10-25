060541d14830fae30826743267cbf5480c637add
sudharsanmaran <smsudharsanmaran@gmail.com>
2023-10-25 13:22:57 +0530

    ammend

--- src/base/models.py
+++ src/base/models.py
@@ -8,14 +8,15 @@ from src.google.models import GoogleToken
 from src.database import Base
 
 
-
-    
-    
 class User(Base, UUIDMixin, TimestampMixin):
     __tablename__ = "users"
-    
+
     email: Mapped[str] = Column(String, nullable=False, unique=True, index=True)
-    access_token: Mapped[str] = Column(String, nullable=False) # incase need to call any admin server's api
+    access_token: Mapped[str] = Column(
+        String, nullable=False
+    )  # incase need to call any admin server's api
     expires_at: Mapped[int] = Column(Integer, nullable=False)
 
-    google_token: Mapped[List[GoogleToken]] = relationship('GoogleToken', back_populates='user', uselist=True)
+    google_token: Mapped[List[GoogleToken]] = relationship(
+        "GoogleToken", back_populates="user", uselist=True
+    )
--- src/google/models.py
+++ src/google/models.py
@@ -8,9 +8,9 @@ from src.database import Base
 
 class GoogleToken(Base, UUIDMixin, TokenInfoMixin, TimestampMixin):
     __tablename__ = "google_tokens"
-    
+
     expires_at: Mapped[datetime.datetime] = Column(DateTime, nullable=False)
-    user = relationship('User', back_populates='google_token')
+    user = relationship("User", back_populates="google_token")
 
     def __repr__(self) -> str:
-        return f'<GoogleToken {self.email} {self.display_name}>'
+        return f"<GoogleToken {self.email} {self.display_name}>"
