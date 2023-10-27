37dff7b34a06f28f6c38ee30f3ba2d7af541becb
sudharsanmaran <smsudharsanmaran@gmail.com>
2023-10-27 15:50:22 +0530

    fix: services

--- src/base/services.py
+++ src/base/services.py
@@ -1,6 +1,7 @@
 import logging
+import uuid
 from fastapi import HTTPException
-from sqlalchemy import and_, select
+from sqlalchemy import and_, select, update
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy.orm.exc import NoResultFound
@@ -11,6 +12,8 @@ from cachetools import cached, TTLCache
 from src.base.models import User
 
 logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.INFO)
 
 cache = TTLCache(maxsize=100, ttl=300)
 
@@ -124,23 +127,21 @@ class ListCreateUpdateRetrieveDeleteService:
         try:
             with self.db.begin():
                 self.db.merge(obj)
-                self.db.flush()
-                self.db.refresh(obj)
             logger.info(f"Updated {self.model_class} with parameters {obj}")
             return obj
         except (IntegrityError, SQLAlchemyError) as e:
             self.handle_sqlalchemy_error(e, self.model_class)
 
-    def bulk_update(self, objs: List) -> List[Query]:
-        try:
-            with self.db.begin():
-                self.db.bulk_update_mappings(self.model_class, objs)
-                self.db.flush()
-                self.db.refresh(objs)
-            logger.info(f"Bulk updated {len(objs)} {self.model_class} records")
-            return objs
-        except (IntegrityError, SQLAlchemyError) as e:
-            self.handle_sqlalchemy_error(e, self.model_class)
+    # def bulk_update(self, objs: List) -> List[Query]:
+    #     try:
+    #         with self.db.begin():
+    #             self.db.bulk_update_mappings(self.model_class, objs)
+    #             self.db.flush()
+    #             self.db.refresh(objs)
+    #         logger.info(f"Bulk updated {len(objs)} {self.model_class} records")
+    #         return objs
+    #     except (IntegrityError, SQLAlchemyError) as e:
+    #         self.handle_sqlalchemy_error(e, self.model_class)
 
     def create_or_update(self, obj, lookup_field) -> Query:
         try:
@@ -148,56 +149,105 @@ class ListCreateUpdateRetrieveDeleteService:
             if existing_record:
                 for key, value in obj.items():
                     setattr(existing_record, key, value)
-            else:
-                existing_record = self.model_class(**obj)
-            return self.update(existing_record)
-        except (IntegrityError, SQLAlchemyError) as e:
-            self.handle_sqlalchemy_error(e, self.model_class)
-
-    def bulk_create_or_update(self, objs: List[dict], lookup_field: str) -> None:
-        try:
-            lookup_values = [obj[lookup_field] for obj in objs]
-
-            existing_records = (
-                self.db.query(self.model_class)
-                .filter(getattr(self.model_class, lookup_field).in_(lookup_values))
-                .all()
-            )
-
-            existing_records_dict = {
-                getattr(record, lookup_field): record for record in existing_records
-            }
-
-            updates = []
-            inserts = []
-
-            for obj in objs:
-                record = existing_records_dict.get(obj[lookup_field])
-                if record:
-                    for key, value in obj.items():
-                        setattr(record, key, value)
-                    updates.append(record)
-                else:
-                    inserts.append(self.model_class(**obj))
-
             with self.db.begin():
-                # Perform bulk update and insert
-                self.db.bulk_update_mappings(
-                    self.model_class, [dict(u) for u in updates]
-                )
-                self.db.bulk_save_objects(inserts)
-
-            logger.info(
-                f"Bulk created or updated {len(objs)} {self.model_class} records"
-            )
+                merged = self.db.merge(existing_record or self.model_class(**obj))
+                self.db.flush()
+                self.db.refresh(merged)
+            logger.info(f"Updated {self.model_class} with parameters {obj}")
+            return merged
         except (IntegrityError, SQLAlchemyError) as e:
             self.handle_sqlalchemy_error(e, self.model_class)
+
+    # def bulk_create_or_update(self, objs: List[dict], lookup_field: str) -> None:
+    #     try:
+    #         lookup_values = [obj[lookup_field] for obj in objs]
+
+    #         existing_records = (
+    #             self.db.query(self.model_class)
+    #             .filter(getattr(self.model_class, lookup_field).in_(lookup_values))
+    #             .all()
+    #         )
+
+    #         existing_records_dict = {
+    #             getattr(record, lookup_field): record for record in existing_records
+    #         }
+
+    #         updates = []
+    #         inserts = []
+
+    #         for obj in objs:
+    #             record = existing_records_dict.get(obj[lookup_field])
+    #             if record:
+    #                 for key, value in obj.items():
+    #                     setattr(record, key, value)
+    #                 updates.append(record)
+    #             else:
+    #                 inserts.append(obj)
+
+    #         self.db.bulk_update_mappings(
+    #             self.model_class, [dict(u) for u in updates]
+    #         )
+    #         self.db.bulk_save_objects(inserts)
+
+    #         logger.info(
+    #             f"Bulk created or updated {len(objs)} {self.model_class} records"
+    #         )
+    #     except (IntegrityError, SQLAlchemyError) as e:
+    #         self.handle_sqlalchemy_error(e, self.model_class)
 
     def delete(self, obj) -> None:
         try:
             with self.db.begin():
                 self.db.delete(obj)
             logger.info(f"Deleted {self.model_class} with parameters {obj}")
+        except (IntegrityError, SQLAlchemyError) as e:
+            self.handle_sqlalchemy_error(e, self.model_class)
+
+    def get_unset_deafult_stmt(self, user_id: uuid.UUID):
+        return (
+            update(self.model_class)
+            .where(
+                and_(
+                    getattr(self.model_class, "user_id") == user_id,
+                    getattr(self.model_class, "default") == True,
+                )
+            )
+            .values(default=False)
+        )
+
+    def unset_and_set_default(self, user_id: uuid.UUID, email: str):
+        unset_stmt = self.get_unset_deafult_stmt(user_id)
+        set_stmt = (
+            update(self.model_class)
+            .where(
+                and_(
+                    self.model_class.user_id == user_id,
+                    self.model_class.email == email,
+                )
+            )
+            .values(default=True)
+        )
+
+        with self.db.begin():
+            self.db.execute(unset_stmt)
+            self.db.execute(set_stmt)
+
+    def unset_default_and_create_or_update(self, obj) -> Query:
+        unset_stmt = self.get_unset_deafult_stmt(obj["user_id"])
+        try:
+            existing_record = self.get(user_id=obj["user_id"], email=obj["email"])
+            if existing_record:
+                for key, value in obj.items():
+                    setattr(existing_record, key, value)
+            with self.db.begin():
+                self.db.execute(unset_stmt)
+                merged = self.db.merge(existing_record or self.model_class(**obj))
+                self.db.flush()
+                self.db.refresh(merged)
+            self.logger.info(
+                f"Created or updated {self.model_class} with parameters {obj}"
+            )
+            return merged
         except (IntegrityError, SQLAlchemyError) as e:
             self.handle_sqlalchemy_error(e, self.model_class)
 
@@ -208,3 +258,4 @@ class UserService(ListCreateUpdateRetrieveDeleteService):
 
     def get_by_email(self, email: str) -> Query:
         return self.get(email=email)
+
